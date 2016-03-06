8d77b6679b199d6cbe3ec30c8f38ba1810c1aebe
santos22 <santosjavier22@gmail.com>
2016-03-06 14:55:45 -0600

    Created a model class for Post object to create and save an object to Parse for an image - image and corresponding caption are no uploading to Parse

--- Instagram/PostViewController.swift
+++ Instagram/PostViewController.swift
@@ -8,10 +8,22 @@
 
 import UIKit
 
-class PostViewController: UIViewController {
-
+class PostViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    var postPreviewImage: UIImage!
+    @IBOutlet weak var postPreviewImageView: UIImageView!
+    @IBOutlet weak var captionField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        vc.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        
+        self.presentViewController(vc, animated: true, completion: nil)
 
         // Do any additional setup after loading the view.
     }
@@ -21,7 +33,23 @@ class PostViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func imagePickerController(picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+            // Get the image captured by the UIImagePickerController
+            let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+            //let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+            postPreviewImageView.image = originalImage
+            
+            // Do something with the images (based on your use case)
+            
+            // Dismiss UIImagePickerController to go back to your original view controller
+            dismissViewControllerAnimated(true, completion: nil)
+    }
 
+    @IBAction func saveImageToParse(sender: AnyObject) {
+        Post.postUserImage(postPreviewImageView.image, withCaption: captionField.text ?? "") { (flag: Bool, error: NSError?) -> Void in}
+        print("CHECK PARSE BRO")
+    }
     /*
     // MARK: - Navigation
 
